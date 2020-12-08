8e4a44f670d2d317bc6e9ff8cdbd2c50c1f6b2a1
brain <chidubembruno22@gmail.com>
2020-12-08 14:32:08 +0000

    added homepage and login page
    I designed an amazing interface

--- js/main.js
+++ js/main.js
@@ -1,1 +1,23 @@
-document.write(hello ) 
+window.addEventListener('scroll',function(){
+    const header=document.querySelector('header');
+    header.classList.toggle("sticky",window.scrollY > 0);
+    const nav=document.querySelector('.nav');
+    nav.classList.toggle("navdisplay",window.scrollY>0);
+   
+    var con=document.querySelector('.con');
+    var second=document.querySelector('#second');
+    var third=document.querySelector('#third');
+   
+    var conPosition=con.getBoundingClientRect().top;
+    var screenPosition=window.innerHeight/1.3;
+    if(conPosition<screenPosition){
+        con.classList.add('conshow');
+        second.classList.add('conshow');
+        third.classList.add('conshow');
+    }
+   
+    
+    
+    
+   
+});
